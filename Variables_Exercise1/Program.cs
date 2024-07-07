b8c1246721ca1dc4709677c92047e894014f5448
Chris Gaudet <chrisgaudet9@gmail.com>
2024-07-07 17:28:35 -0500

    Add project files.

---
+++ Variables_Exercise1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Declaring and Initializing Variables
+string name = "Chris";
+int age = 33;
+char middleInitial = 'R';
+bool isOver18 = true;
+double currentTemp = 98.9;
+decimal currentPrice = 29.99m;
+
+//Writing the variables to the concole
+Console.WriteLine($"Name: {name}\n" +
+                  $"Age: {age}\n" +
+          $"Middle Initial: {middleInitial}\n" +
+            $"Is Over 18: {isOver18}\n" +
+            $"Current Temp: {currentTemp}\n" +
+            $"Current Price: {currentPrice}\n");
